a208fecdcc076a943f1fd8c3eeb52e7b5b069116
Shubham Utwal <shubhamutwal@gmail.com>
2016-07-10 01:36:44 +0530

    Removed commented code

--- dao/src/main/java/com/hackathon/transit/common/App.java
+++ dao/src/main/java/com/hackathon/transit/common/App.java
@@ -14,40 +14,13 @@ public class App {
     public static void main(String[] args) {
         ApplicationContext appContext =
                 new ClassPathXmlApplicationContext("spring/config/BeanLocations.xml");
-        /*
-
-        StopBo stopBo = (StopBo) appContext.getBean("stopBo");
-        List stopsList = stopBo.findBySrcAndDestLatLong(0.0, 0.0, 0.0, 703.0);
-
-        for(int i=0;i<stopsList.size();i++) {
-
-           Stop stop=(Stop)stopsList.get(i);
-           StopTimeBo stopTimeBo = (StopTimeBo) appContext.getBean("stopTimeBo");
-           List stopTimeList = stopTimeBo.findByStopId(stop.getStopId());
-           for(int j=0;j<stopTimeList.size();j++){
-               //System.out.println((StopTime)stopTimeList.get(j));
-               StopTime stopTime=(StopTime)stopTimeList.get(j);
-               TripBo tripBo=(TripBo) appContext.getBean("tripBo");
-               List tripList = tripBo.findByTripId(stopTime.getTripId());
-
-               for(int k=0;k<tripList.size();k++){
-                   Trip trip=(Trip)tripList.get(k);
-                   RouteBO routeBO=(RouteBO)appContext.getBean("routeBo");
-                   List routeList = routeBO.findByRouteId(trip.getRouteId());
-                   for(int p=0;p<routeList.size();p++){
-                       System.out.println((Route)routeList.get(p));
-                   }
-               }
-           }
-        }
-        */
 
         Double sourceLat = 18.4287248157414;
         Double sourceLon = 18.5665579874744;
 
         Double destLat = 73.756157988755;
         Double destLon = 73.9084024898728;
-        
+
         StarksBo starksBo = (StarksBo) appContext.getBean("starksBo");
         List routeList = starksBo.getBusListBetweenSourceAndDestination(sourceLat, sourceLon, destLat, destLon);
         for (Object route : routeList) {
